b81667a4c68226770c1a499e6a7c3a27a701bee1
Rasujon3 <rasujon3@gmail.com>
2022-09-01 03:56:29 +0600

    6.10 Related video features

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import tagsReduce from "../features/tags/tagsSlice";
 import videosReducer from "../features/videos/vidoesSlice";
 import videoReducer from "../features/video/videoSlice";
+import relatedVideoReducer from "../features/relatedVideos/relatedVideosSlice";
 
 export const store = configureStore({
   reducer: {
     videos: videosReducer,
     tags: tagsReduce,
     video: videoReducer,
+    relatedVideos: relatedVideoReducer,
   },
 });
