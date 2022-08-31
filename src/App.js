b6494b1c8f2b8cc0c8ecb791a314957829c83f98
Rasujon3 <rasujon3@gmail.com>
2022-09-01 00:07:20 +0600

    6.4 Setup Routing - react-rouer-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home";
 import Video from "./pages/Video";
 
 function App() {
-  return <Video />;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        {/* page level content */}
+        <Route path="/" element={<Home />} />
+        <Route path="/videos/:vidoeId" element={<Video />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,33 +1,25 @@
 import React from "react";
 import Player from "../components/Description/Player";
 import VideoDescription from "../components/Description/VideoDescription";
-import Footer from "../components/Footer/Footer";
 import RelatedVideoList from "../components/List/RelatedVideoList";
-import Navbar from "../components/Navbar/Navbar";
 
 const Video = () => {
   return (
-    <>
-      <Navbar />
-
-      <section class="pt-6 pb-20">
-        <div class="mx-auto max-w-7xl px-2 pb-20 min-h-[400px]">
-          <div class="grid grid-cols-3 gap-2 lg:gap-8">
-            <div class="col-span-full w-full space-y-8 lg:col-span-2">
-              {/* <!-- video player --> */}
-              <Player />
-              {/* <!-- video description --> */}
-              <VideoDescription />
-            </div>
-
-            {/* <!-- related videos --> */}
-            <RelatedVideoList />
+    <section class="pt-6 pb-20">
+      <div class="mx-auto max-w-7xl px-2 pb-20 min-h-[400px]">
+        <div class="grid grid-cols-3 gap-2 lg:gap-8">
+          <div class="col-span-full w-full space-y-8 lg:col-span-2">
+            {/* <!-- video player --> */}
+            <Player />
+            {/* <!-- video description --> */}
+            <VideoDescription />
           </div>
-        </div>
-      </section>
 
-      <Footer />
-    </>
+          {/* <!-- related videos --> */}
+          <RelatedVideoList />
+        </div>
+      </div>
+    </section>
   );
 };
 
