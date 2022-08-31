af6addc6f4abc2db278cb5946234dc628a7066fb
Rasujon3 <rasujon3@gmail.com>
2022-09-01 01:56:59 +0600

    6.7 tags Features 1 - Create tags slice and tags API

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import tagsReduce from "../features/tags/tagsSlice";
 import videosReducer from "../features/videos/vidoesSlice";
 
 export const store = configureStore({
   reducer: {
     videos: videosReducer,
+    tags: tagsReduce,
   },
 });
